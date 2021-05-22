bbf955d206fd0bb6b3e0387b4e397ee0005c0022
JitinKansal <jkansal312@gmail.com>
2021-05-23 01:39:50 +0530

    adding googleapis

--- public/js/send_mails.js
+++ public/js/send_mails.js
@@ -1,34 +1,53 @@
 const nodemailer = require("nodemailer");
-// const ejs = require('ejs');
+
+const {google} = require('googleapis');
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECTED_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECTED_URI);
+oAuth2Client.setCredentials({refresh_token:REFRESH_TOKEN});
 
 async function Notify_through_mail(tosend,mailsubject,textToSend) {
+  try{
 
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.GMAIL_ID, 
-      pass: process.env.GMAIL_PASSWORD, 
-    },
-  });
+      const ACESS_TOKEN = await oAuth2Client.getAccessToken();
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: 'OAuth2',
+          user: process.env.GMAIL_ID, 
+          clientId: CLIENT_ID,
+          clientSecret: CLIENT_SECRET,
+          refreshToken: REFRESH_TOKEN,
+          acessToken: ACESS_TOKEN,
+        },
+      });
 
-  var options = {
-    from: `"vaccineslotsnotifier"<${process.env.GMAIL_ID}>`, 
-    to: tosend, 
-    subject: mailsubject, 
-    html: textToSend ,
-  };
+      var options = {
+        from: `"vaccineslotsnotifier"<${process.env.GMAIL_ID}>`, 
+        to: tosend, 
+        subject: mailsubject, 
+        html: textToSend ,
+      };
 
 
-transporter.sendMail(options,function(error,info){
-    if(error)
-    {
-        console.log(error);
-    }
-    else
-    {
-        console.log("Email sent" + info.response);
-    }
-});
+    transporter.sendMail(options,function(error,info){
+        if(error)
+        {
+            console.log(error);
+        }
+        else
+        {
+            console.log("Email sent" + info.response);
+        }
+    });
+  }
+  catch(err){
+    console.log(err);
+  }
 };
 
 module.exports = Notify_through_mail;
